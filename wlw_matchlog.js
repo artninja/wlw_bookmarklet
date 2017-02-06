8c7a5ac89afe9030dbdcebb3109d1cf321bef9e8
tomo <u5amin@chu-bu.me>
2017-02-07 07:21:03 +0900

    キャスト別対戦成績を表示ボタンの動作設定をjQuery使った形式で書き直し メイン処理を関数でまとめた 1つのURLしかチェックしなくてよくなったのでURLチェック関数を削除し、URLチェックはメイン処理内で実施

--- wlw_matchlog.js
+++ wlw_matchlog.js
@@ -1,20 +1,22 @@
-// URL判定関数
-function chk_url(urlstr){
-  var matchlog_sub = /^https:\/\/wonderland-wars.net\/matchlog_sub.html$/;
+// エラーメッセージ表示関数
+function disp_err(err_code){
+  var error_mesg = [
+    "不明なエラー",             // Error No:0
+    "複数回実行しないでください",   // Error No:1
+    "URLチェックに失敗しました。対戦履歴ページで実行していますか?"
+  ];
 
-  var ret = "error";
+  console.log("Error No:" + err_code);
 
-  if(matchlog_sub.test(urlstr)){
-  	ret = "matchlog_sub";
+  // エラーコード未設定、またはエラーメッセージのサイズ以上のエラーコードを渡された場合に「不明なエラー」(エラーコード0)にする。
+  if(err_code == undefined || error_mesg.length < err_code){
+    n = 0;
   }
-  else {
-  	ret = "error";
-  }
 
-  return ret;
+  alert(error_mesg[err_code]);
 }
 
-// ページ取得・解析関数
+// ページ取得
 function get_page(request_info_array, func){
   // ページ取得結果格納用配列
   var results = [];
@@ -38,12 +40,8 @@ function analyze_matchlog_sub(str){
   return ret;
 }
 
-function exec(btn,url,page_cnt){
-  // 複数回実行できないようボタンを非表示に
-  btn.style.visibility = "hidden";
-
-  var date = url.slice(-10)
-
+//
+function exec(target,url,page_cnt){
   var request_info_array = [];
   for (var i = 0; i < page_cnt; i++) {
     request_info_array.push({
@@ -101,40 +99,60 @@ function exec(btn,url,page_cnt){
       str += "</tr>";
     }
     str += "</table>";
-    $('#'+date+'').append(str);
+    $(target).append(str);
   }).fail(function(){
     alert("取得失敗したページあります。");
   });
 }
 
 // メイン処理
-// 実行したページのURLのチェックを行い、実行する処理モードを決定
-var exec_mode = chk_url(location.href);
-console.log($("#bml".length));
+(function main(){
+  var match_str = /^https:\/\/wonderland-wars.net\/matchlog_sub.html$/;
+  if(match_str.test(location.href)){
+  //URLチェックを行い、該当ページであれば以下の処理を実行する。
 
-if(!($("#bml").size())){
-  $("head").append("<meta id=\"bml\" />");
-  // 詳細ページリクエスト用データを格納するための配列
-  var request_info_array = [];
+    $(".block_matchlog_list").each(function(i){
+      // 全国対戦を行っているかチェック
+      var match_block = ($(this).find(".block_matchlog_match"));
+      if(!match_block[0]){
+        // 行っていなかった場合、次のループへ(continue)
+        return true;
+      }
 
-  $(".block_matchlog_list").each(function(i){
-    // 全国対戦を行っているかチェック
+      // 戦績詳細ページの URL を取得
+      var url = $(match_block).children("a").attr("href");
 
-    var match_block = ($(this).find(".block_matchlog_match"))
+      // ToDo:ここに URLの取得に失敗したときの処理を追加
 
-    if(!match_block[0]){
-      return true;
-    }
+      // URL の末尾が日付になっているので、それを取得
+      var date = url.slice(-10)
 
-    var url = $(match_block).children("a").attr("href");
-    var date = url.slice(-10)
-    var play_cnt = $(match_block).find(".matchlog_list_total").html();
-    var win_cnt = $(match_block).find(".matchlog_list_win").html();
-    var page_cnt = Math.ceil(play_cnt/10)
-    var str = "<div id="+date+"><div>全体勝率:" + ((win_cnt/play_cnt)*100).toFixed(1) + "%</div><a onclick='javascript:exec(this,\"" + url + "\"," + page_cnt + ");'>キャスト別対戦成績を表示する</a></div>"
-    $(this).find(".block_matchlog_match").after(str);
-  });
-}
-else{
-  alert("複数回実行しないでください");
-}
+      // 全国対戦の対戦回数を取得
+      var play_cnt = $(match_block).find(".matchlog_list_total").html();
+
+      // 戦績詳細ページのページ数を算出(2017.2現在、10戦で1ページ増える計算)
+      var page_cnt = Math.ceil(play_cnt/10)
+
+      // 全国対戦の勝利数を取得
+      var win_cnt = $(match_block).find(".matchlog_list_win").html();
+
+      var str = "<div>";
+      str += "<div>全体勝率:" + ((win_cnt/play_cnt)*100).toFixed(1)+"%</div>";
+      str += "<input type=button value='キャスト別対戦成績を表示' />"
+      str += "</div>";
+
+      $(str).insertAfter($(this).find(".block_matchlog_match")).on('click','input',function(){
+        // 複数回実行防止のため実行ボタンを非表示にする
+        $(this).hide();
+        // 無効にするだけならこっち
+        //$(this).prop("disabled", true);
+
+        target = $(this).parent();
+        exec(target, url, page_cnt);
+      });
+    });
+  }
+  else{
+    alert("URLチェックに失敗しました。対戦履歴ページで実行していますか?");
+  }
+})();
